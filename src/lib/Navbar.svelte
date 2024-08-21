8f301b7f4f8fd64bf5fa5b0f217eda06c74d8498
Altanfiz <altanfizgenity@gmail.com>
2024-08-21 15:01:23 +0700

    navbar:style: add base layout style for nav

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -24,3 +24,24 @@
     {/each}
   </ul>
 </nav>
+
+<style>
+  nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 96px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    place-items: center start;
+    z-index: 1000;
+    background-color: hsl(270, 45%, 5%);
+    gap: var(--itemGap2);
+    padding-block: var(--whiteSpace);
+  }
+  .logo {
+    width: 30px;
+    height: 30px;
+  }
+</style>
