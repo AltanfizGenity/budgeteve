f3212125490f482c3bdc9b2eb9fc8b3d7680dce5
Altanfiz <altanfizgenity@gmail.com>
2024-08-21 15:34:19 +0700

    added IconBtn core component

---
+++ src/lib/btns/icon-btns/IconBtn.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let classname = "";
+</script>
+
+<button class={`icon-btn ${classname}`}>
+  <slot />
+</button>
+
+<style>
+  .icon-btn {
+    width: 16px;
+    height: 16px;
+    background-color: transparent;
+    border: none;
+  }
+</style>
