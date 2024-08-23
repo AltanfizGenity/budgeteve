585f23584267483857a8b13c522c5f4e9ca112a2
Altanfiz <altanfizgenity@gmail.com>
2024-08-23 10:42:05 +0700

    added icon buttons entry point

---
+++ src/lib/btns/icon-btns/index.js
@@ -0,0 +1,4 @@
+export { default as BalanceAccountsBtn } from "./BalanceAccountsBtn.svelte";
+export { default as AddBtn } from "./AddBtn.svelte";
+export { default as RecordsBtn } from "./RecordsBtn.svelte";
+export { default as HomeBtn } from "./HomeBtn.svelte";
