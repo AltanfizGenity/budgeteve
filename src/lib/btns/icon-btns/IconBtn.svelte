6580c6cd7a674598f14d189ad3af105fe4dadb7e
Altanfiz <altanfizgenity@gmail.com>
2024-08-23 11:40:25 +0700

    [style]: add props and style to IconBtn

--- src/lib/btns/icon-btns/IconBtn.svelte
+++ src/lib/btns/icon-btns/IconBtn.svelte
@@ -1,16 +1,23 @@
 <script>
   export let classname = "";
+  export let size = 24;
 </script>
 
-<button class={`icon-btn ${classname}`}>
+<button class={`icon-btn ${classname}`} style="--size: {size}px;">
   <slot />
 </button>
 
 <style>
   .icon-btn {
-    width: 16px;
-    height: 16px;
+    width: var(--size);
+    height: var(--size);
     background-color: transparent;
     border: none;
+
+    &:hover {
+      color: var(--primaryColor);
+      box-shadow: 0 0 32px var(--primaryColor);
+      cursor: pointer;
+    }
   }
 </style>
