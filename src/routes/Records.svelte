ef55dd71331f00806daccfe7aa85e53eb580ab47
Altanfiz <altanfizgenity@gmail.com>
2024-08-23 17:06:03 +0700

    [lab]: display record proto data

--- src/routes/Records.svelte
+++ src/routes/Records.svelte
@@ -1,6 +1,17 @@
+<script>
+  import { recordDataProto } from "../data/dataLab";
+</script>
+
 <section class="records">
   <h1>Records</h1>
   <div class="record-history">
-    <h1>record history</h1>
+    {#each recordDataProto as record}
+      <div class="record">
+        <div>record id: {record.id}</div>
+        <div>type: {record.type}</div>
+        <div>date: {record.date}</div>
+        <div>amount: {record.amount}</div>
+      </div>
+    {/each}
   </div>
 </section>
