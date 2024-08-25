660f55f15e5cbc8756371e0f2b67ee4c9ee055ec
Altanfiz <altanfizgenity@gmail.com>
2024-08-25 17:36:34 +0700

    [refactor]: change hard-coded form data fetching to form API

--- src/lib/forms/RecordForm.svelte
+++ src/lib/forms/RecordForm.svelte
@@ -6,12 +6,21 @@
   let category = null;
   let account = "cash";
   let date = null;
-  let amount = 0;
+  let amount = null;
   let note = "";
 
-  function addRecord() {
-    let formData = [type, category, account, date, amount, note];
-    let newRecord = createRecordData(...formData);
+  function addRecord(event) {
+    let formData = new FormData(event.target);
+
+    let newRecord = createRecordData(
+      formData.get("type"),
+      formData.get("category"),
+      formData.get("account"),
+      formData.get("date"),
+      formData.get("amount"),
+      formData.get("note")
+    );
+
     currentRecords.update((val) => [...val, newRecord]);
   }
 </script>
@@ -19,36 +28,36 @@
 <form class="record-form" on:submit|preventDefault={addRecord}>
   <div class="input-group">
     <label for="">Type</label>
-    <select bind:value={type}>
+    <select bind:value={type} name="type">
       <option value="income">income</option>
       <option value="expense">expense</option>
     </select>
   </div>
   <div class="input-group">
     <label for="">Category</label>
-    <select bind:value={category}>
+    <select bind:value={category} name="category">
       <option value="salary">salary</option>
       <option value="rent">rent</option>
     </select>
   </div>
   <div class="input-group">
     <label for="">Account</label>
-    <select bind:value={account}>
+    <select bind:value={account} name="account">
       <option value="cash">cash</option>
       <option value="bank">bank</option>
     </select>
   </div>
   <div class="input-group">
     <label for="">Date</label>
-    <input type="date" bind:value={date} />
+    <input type="date" bind:value={date} name="date" />
   </div>
   <div class="input-group">
     <label for="">Amount</label>
-    <input type="number" bind:value={amount} />
+    <input type="number" bind:value={amount} name="amount" placeholder="0" />
   </div>
   <div class="input-group">
     <label for="">Note</label>
-    <textarea bind:value={note}></textarea>
+    <textarea bind:value={note} name="note"></textarea>
   </div>
   <button>add record</button>
 </form>
