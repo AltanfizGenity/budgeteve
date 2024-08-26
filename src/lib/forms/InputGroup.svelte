fce6e4ca4e2228c08a5eaa004b28a5a1d24b98e9
Altanfiz <altanfizgenity@gmail.com>
2024-08-26 15:24:50 +0700

    [refactor]: change input group to InputGroup component

---
+++ src/lib/forms/InputGroup.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let name;
+  export let label = "";
+</script>
+
+<div class="input-group">
+  <label for={name}>{label || name}</label>
+  <slot />
+</div>
--- src/lib/forms/RecordForm.svelte
+++ src/lib/forms/RecordForm.svelte
@@ -1,6 +1,7 @@
 <script>
   import { createRecordData } from "../../data/dataLab";
   import { currentRecords } from "../../store/appdata";
+  import InputGroup from "./InputGroup.svelte";
 
   let type = "income";
   let category = null;
@@ -26,38 +27,32 @@
 </script>
 
 <form class="record-form" on:submit|preventDefault={addRecord}>
-  <div class="input-group">
-    <label for="">Type</label>
-    <select bind:value={type} name="type">
+  <InputGroup name="type">
+    <select bind:value={type} name="type" id="type">
       <option value="income">income</option>
       <option value="expense">expense</option>
     </select>
-  </div>
-  <div class="input-group">
-    <label for="">Category</label>
-    <select bind:value={category} name="category">
+  </InputGroup>
+  <InputGroup name="category">
+    <select bind:value={category} name="category" id="category">
       <option value="salary">salary</option>
       <option value="rent">rent</option>
     </select>
-  </div>
-  <div class="input-group">
-    <label for="">Account</label>
-    <select bind:value={account} name="account">
+  </InputGroup>
+  <InputGroup name="account">
+    <select bind:value={account} name="account" id="account">
       <option value="cash">cash</option>
       <option value="bank">bank</option>
     </select>
-  </div>
-  <div class="input-group">
-    <label for="">Date</label>
-    <input type="date" bind:value={date} name="date" />
-  </div>
-  <div class="input-group">
-    <label for="">Amount</label>
-    <input type="number" bind:value={amount} name="amount" placeholder="0" />
-  </div>
-  <div class="input-group">
-    <label for="">Note</label>
-    <textarea bind:value={note} name="note"></textarea>
-  </div>
+  </InputGroup>
+  <InputGroup name="date">
+    <input type="date" bind:value={date} name="date" id="date" />
+  </InputGroup>
+  <InputGroup name="amount">
+    <input type="number" bind:value={amount} name="amount" placeholder="0" id="amount" />
+  </InputGroup>
+  <InputGroup name="note">
+    <textarea bind:value={note} name="note" id="note"></textarea>
+  </InputGroup>
   <button>add record</button>
 </form>
