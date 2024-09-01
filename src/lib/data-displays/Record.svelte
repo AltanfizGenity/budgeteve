89f7c2457b7999a8231841cccaad47c829152025
Altanfiz <altanfizgenity@gmail.com>
2024-09-01 11:03:39 +0700

    [lab]: add date to record details

--- src/lib/data-displays/Record.svelte
+++ src/lib/data-displays/Record.svelte
@@ -1,5 +1,11 @@
 <script>
-  export let category, amount;
+  export let category, amount, date;
+
+  const parsedDate = new Date(date).toLocaleDateString("en-gb", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 </script>
 
 <div class="record">
@@ -7,6 +13,9 @@
     <div class="icon">icon</div>
     <div class="category">
       {category}
+    </div>
+    <div class="date">
+      {parsedDate}
     </div>
   </div>
   <div class="amount">{amount}</div>
