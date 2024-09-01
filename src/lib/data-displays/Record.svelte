31aa9f21df58219550644718faf14ae4054b1ca4
Altanfiz <altanfizgenity@gmail.com>
2024-09-01 10:35:41 +0700

    [style]: add base style for Record

--- src/lib/data-displays/Record.svelte
+++ src/lib/data-displays/Record.svelte
@@ -11,3 +11,20 @@
   </div>
   <div class="amount">{amount}</div>
 </div>
+
+<style>
+  .record {
+    display: flex;
+    width: 100%;
+    padding: var(--contentPadding);
+    justify-content: space-between;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.05);
+  }
+
+  .details {
+    display: flex;
+    align-items: center;
+    gap: var(--whiteSpace);
+  }
+</style>
