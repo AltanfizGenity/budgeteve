b141392e96c5d6b87fc5765d2b0b6df466c0dffe
Altanfiz <altanfizgenity@gmail.com>
2024-09-04 06:58:54 +0700

    [lab]: add accounts store

--- src/store/appdata.js
+++ src/store/appdata.js
@@ -1,3 +1,8 @@
 import { writable } from "svelte/store";
+import { defaultAccountsProto } from "../data/dataLab";
 
 export let currentRecords = writable([]);
+export let currentAccounts = writable([]);
+
+// LAB: Mock data from data lab
+currentAccounts.set(defaultAccountsProto);
