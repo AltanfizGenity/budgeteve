6a2ab1860c4ba0aa8d192472a32e763933956ad8
Altanfiz <altanfizgenity@gmail.com>
2024-09-05 21:30:11 +0700

    [style]: added icon proto to record bar

--- src/lib/data-displays/Record.svelte
+++ src/lib/data-displays/Record.svelte
@@ -1,18 +1,17 @@
 <script>
-  import { parseDateDefault } from "../../utils/date";
-  export let category, amount, date;
-
-  const parsedDate = parseDateDefault(date);
+  import RiSalary from "../icons/RI-Salary.svelte";
+  export let category, amount;
 </script>
 
 <div class="record">
   <div class="details">
-    <div class="icon">icon</div>
+    <div class="badge">
+      <div class="icon">
+        <RiSalary />
+      </div>
+    </div>
     <div class="category">
       {category}
-    </div>
-    <div class="date">
-      {parsedDate}
     </div>
   </div>
   <div class="amount">{amount}</div>
@@ -22,7 +21,8 @@
   .record {
     display: flex;
     width: 100%;
-    padding: var(--contentPadding);
+    padding: 1.5rem;
+    border-radius: var(--radius);
     justify-content: space-between;
     align-items: center;
     background-color: rgba(255, 255, 255, 0.05);
@@ -31,6 +31,29 @@
   .details {
     display: flex;
     align-items: center;
-    gap: var(--whiteSpace);
+    gap: 1rem;
+  }
+
+  .badge {
+    background-color: rgba(151, 83, 223, 0.2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 36px;
+    height: 36px;
+    border-radius: var(--radius);
+  }
+
+  .icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 18px;
+    height: 18px;
+    color: var(--accentColor);
+  }
+
+  .category {
+    text-transform: capitalize;
   }
 </style>
