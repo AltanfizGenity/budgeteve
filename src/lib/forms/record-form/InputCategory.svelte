ff1805509d17476155d9daf309da2cbf2dc50115
Altanfiz <altanfizgenity@gmail.com>
2024-09-07 15:59:00 +0700

    [refactor]: InputCategory show category data instead hard coded

--- src/lib/forms/record-form/InputCategory.svelte
+++ src/lib/forms/record-form/InputCategory.svelte
@@ -1,11 +1,13 @@
 <script>
+  import { defaultCategories } from "../../../data/dataLab";
   import InputGroup from "../InputGroup.svelte";
   let category = "salary";
 </script>
 
 <InputGroup name="category">
   <select bind:value={category} name="category" id="category">
-    <option value="salary">salary</option>
-    <option value="rent">rent</option>
+    {#each defaultCategories["income"] as { categoryName }}
+      <option value={categoryName}>{categoryName}</option>
+    {/each}
   </select>
 </InputGroup>
