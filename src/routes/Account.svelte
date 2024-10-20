022f4577f5be9b5c622997bac1bfbcf3bb3f6667
Altanfiz <altanfizgenity@gmail.com>
2024-10-20 11:53:57 +0700

    [lab]: add basic style for account page

--- src/routes/Account.svelte
+++ src/routes/Account.svelte
@@ -4,7 +4,7 @@
 
 <section class="account-page">
   <h2>My account</h2>
-  <ul>
+  <ul class="account-list">
     {#each $currentAccounts as account}
       <li>
         <div class="account">
@@ -14,8 +14,33 @@
       </li>
     {/each}
   </ul>
-  <button class="btn primary-btn">new account</button>
+  <button class="btn primary new-account-btn">new account</button>
 </section>
 
 <style>
+  .account-page {
+    display: flex;
+    width: 40%;
+    flex-direction: column;
+    gap: var(--itemGap2);
+  }
+
+  .account-list {
+    display: flex;
+    flex-direction: column;
+    gap: var(--itemGap1);
+  }
+
+  .account {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .new-account-btn {
+    padding: 0.5rem 0.8rem;
+    width: 200px;
+    background-color: var(--primaryColor);
+  }
 </style>
