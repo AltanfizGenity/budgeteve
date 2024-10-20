a753c4dd156e5ad9b4457a52a7a01d181fe2822e
Altanfiz <altanfizgenity@gmail.com>
2024-10-20 11:44:54 +0700

    [lab]: create account page layout

---
+++ src/routes/Account.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { currentAccounts } from "../store/appdata";
+</script>
+
+<section class="account-page">
+  <h2>My account</h2>
+  <ul>
+    {#each $currentAccounts as account}
+      <li>
+        <div class="account">
+          <p class="account-name">{account.name}</p>
+          <p class="account-amount">{account.amount}</p>
+        </div>
+      </li>
+    {/each}
+  </ul>
+  <button class="btn primary-btn">new account</button>
+</section>
+
+<style>
+</style>
